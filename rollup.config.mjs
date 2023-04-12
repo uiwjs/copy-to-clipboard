eb10915616284f6fb09193c13e480cdc67cd1e0e
jaywcjlove <398188662@qq.com>
2023-04-12 11:04:07 +0800

    chore(deps): Update dependency rollup to v3.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,11 @@
+import { createRequire } from 'module';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
-import banner from 'bannerjs';
+import terser from '@rollup/plugin-terser';
+import * as banner from 'bannerjs';
 
-import pkg from './package.json';
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
 
 export default [
   // browser-friendly UMD build
